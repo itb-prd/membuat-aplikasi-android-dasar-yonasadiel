3e9346b880606b8159209f929fad5359e7f5f3d8
yonasadiel <yonazadielwiguna@gmail.com>
2016-11-11 13:00:22 +0700

    First Commit

---
+++ app/src/main/java/com/example/ya_/myapplication/AboutDialog.java
@@ -0,0 +1,28 @@
+package com.example.ya_.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by YA_ on 11/9/2016.
+ */
+
+public class AboutDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder theDialog = new AlertDialog.Builder(getActivity());
+
+        theDialog.setTitle("About");
+        theDialog.setMessage("To Do List v. 1.0.2\nMade by YA_");
+        theDialog.setPositiveButton("OK", null);
+
+        /*return super.onCreateDialog(savedInstanceState);*/
+        return theDialog.create();
+    }
+}
---
+++ app/src/main/java/com/example/ya_/myapplication/deleteScreen.java
@@ -0,0 +1,35 @@
+package com.example.ya_.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by YA_ on 11/10/2016.
+ */
+
+public class deleteScreen extends AppCompatActivity {
+
+    Intent lastIntent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String ListPicked = "To Do deleted";
+        Toast.makeText(this, ListPicked, Toast.LENGTH_LONG).show();
+
+
+        lastIntent = getIntent();
+
+        int index = lastIntent.getExtras().getInt("Index of ToDoList");
+
+        Intent goBack = new Intent();
+        goBack.putExtra("Index of ToDoList",index);
+        setResult(RESULT_OK, goBack);
+
+        finish();
+    }
+}
